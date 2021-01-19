8d52e57d109b5f86067f35a3e0475ff01104c2f7
hjhorsting <77336158+hjhorsting@users.noreply.github.com>
2021-01-19 14:49:23 +0100

    Add files via upload

---
+++ PythonBasics.py
@@ -0,0 +1,13 @@
+# 1. Write a code that outputs your name and surname in one line.
+print("Harald Horsting")
+# or:
+print("Harald", "Horsting")
+
+# 2. Write a 1-line code that outputs your name and surname in two separate lines by using
+#    a single print() call.
+print("Harald \n Horsting")
+
+# 3. Use print() function that returns the following text. 
+# Notice the quotes in the text. Those quotes should be seen in the output!
+# `I don't want to be an "artist". I want to be a "Data Scientist."`
+print('''`I don't want to be an "artist". I want to be a "Data Scientist."`''')
