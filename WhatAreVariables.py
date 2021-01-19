e335a7d05d82df24d0e08fe4eb6010afc7589fc8
hjhorsting <77336158+hjhorsting@users.noreply.github.com>
2021-01-19 17:46:34 +0100

    Add files via upload

---
+++ WhatAreVariables.py
@@ -0,0 +1,17 @@
+# 1. Define two variables as x and y. Assign 1 to x and '1' to y. 
+# Then check if two variables have the same value and type.
+x = 1
+y = "1"
+
+print("Value of x:", x,". Type of x:", type(x))
+print("Value of y:", y,". Type of y:", type(y))
+print("So x and y have the same value but different type.")
+
+
+# 2. Change the type of variable y and make the two variables the same.
+y = 1
+
+print("Value of x:", x,". Type of x:", type(x))
+print("Value of y:", y,". Type of y:", type(y))
+print("Now x and y have the same value and type.")
+
